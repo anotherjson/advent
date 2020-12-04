d896a6ce9c2b9932777c53b3531a747f9b7b2184
Jason Bellegarde <main@jasons-air.lan>
2020-12-03 23:34:35 -0700

    finished day 1 script

--- src/aoc_script_day_1.py
+++ src/aoc_script_day_1.py
@@ -11,6 +11,15 @@ dir_file = "/".join([dir_main, "assets"])
 file_name = "/".join([dir_file, "aoc_data_day_1.csv"])
 
 # Functions
+def make_iterations(data, col_names, col_add_name="add", add_value=2020):
+    output = (
+        pd.DataFrame(list(itertools.product(*data)), columns=col_names)
+        .astype(int)
+    )
+
+    output[col_add_name] = output.sum(axis=1)
+
+    return output[output[col_add_name]==add_value]
 
 # Assumptions
 
@@ -19,8 +28,11 @@ with open(file_name, newline='') as csvfile:
     reader = csv.reader(csvfile)
     rows = [row[0] for row in reader]
 
-data = pd.DataFrame(list(itertools.product(rows, rows))).astype(int)
-data = data.rename({0: "value_1", 1: "value_2"}, axis=1)
-data["add"] = data["value_1"] + data["value_2"]
-data["times"] = data["value_1"] * data["value_2"]
-print(data[data["add"]==2020])
+data_2 = make_iterations([rows, rows], col_names=["value_1", "value_2"])
+data_2["multple"] = data_2["value_1"] * data_2["value_2"]
+print(data_2.iloc[0])
+print("----")
+data_3 = make_iterations([rows, rows, rows],
+                         col_names=["value_1", "value_2", "value_3"])
+data_3["multiple"] = data_3["value_1"] * data_3["value_2"] * data_3["value_3"]
+print(data_3.iloc[0])
