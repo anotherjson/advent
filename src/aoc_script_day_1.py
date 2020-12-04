335394d16e9ec78d1d38e1e310c9a71f9e0479ee
Jason Bellegarde <main@jasons-air.lan>
2020-12-04 09:04:15 -0700

    cleaned up iterations function

--- src/aoc_script_day_1.py
+++ src/aoc_script_day_1.py
@@ -5,34 +5,40 @@ import itertools
 import csv
 import os
 
-# Create paths
-dir_main = os.getcwd()
-dir_file = "/".join([dir_main, "assets"])
-file_name = "/".join([dir_file, "aoc_data_day_1.csv"])
-
 # Functions
 def make_iterations(data, col_names, col_add_name="add", add_value=2020):
-    output = (
+    temp = (
         pd.DataFrame(list(itertools.product(*data)), columns=col_names)
         .astype(int)
     )
 
-    output[col_add_name] = output.sum(axis=1)
+    temp[col_add_name] = temp.sum(axis=1)
+    output_1 = temp[temp[col_add_name]==add_value].iloc[0]
 
-    return output[output[col_add_name]==add_value]
+    output_2 = 0
+    for row, value in enumerate(output_1[:-1]):
+        if row == 0:
+            output_2 += value
+
+        else:
+            output_2 += output_2 * value
+
+    return output_1, output_2
 
 # Assumptions
+dir_name = "assets"
+file_name = "aoc_data_day_1.csv"
+
+# Create paths
+dir_main = os.getcwd()
+file_path = "/".join([dir_main, dir_name, file_name])
 
 # Import data
-with open(file_name, newline='') as csvfile:
+with open(file_path, newline='') as csvfile:
     reader = csv.reader(csvfile)
     rows = [row[0] for row in reader]
 
-data_2 = make_iterations([rows, rows], col_names=["value_1", "value_2"])
-data_2["multple"] = data_2["value_1"] * data_2["value_2"]
-print(data_2.iloc[0])
+print(make_iterations([rows, rows], col_names=["value_1", "value_2"]))
 print("----")
-data_3 = make_iterations([rows, rows, rows],
-                         col_names=["value_1", "value_2", "value_3"])
-data_3["multiple"] = data_3["value_1"] * data_3["value_2"] * data_3["value_3"]
-print(data_3.iloc[0])
+print(make_iterations([rows, rows, rows],
+                      col_names=["value_1", "value_2", "value_3"]))
